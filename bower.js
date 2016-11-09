d482384c492a5966636f58fd8fbb633da55885e1
JGarval <javiergarval@gmail.com>
2016-11-09 19:05:05 +0100

    Installed dependencies.

---
+++ bower.js
@@ -0,0 +1,10 @@
+{
+     "name" : "meanCMS",
+     "version" : "0.0.1",
+     "author" : "javiergarval"
+     "license" : "MIT",
+     "dependencies" : {
+          "font-awesome" : "*",
+          "normalize-css" : "*"
+     }
+}
